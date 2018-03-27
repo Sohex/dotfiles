305d8359703f9c2ed73413abc076ff2d5ea6d248
Conor Futro <cfutro@localhost.localdomain>
2018-03-27 01:48:36 -0600

    minor changes

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 powerline-daemon -q
-set fish_function_path $fish_function_path "/usr/lib/python2.7/site-packages/powerline/bindings/fish"
+set fish_function_path $fish_function_path "/home/cfutro/.local/lib/python2.7/site-packages/powerline/bindings/fish"
 powerline-setup
 
 set fish_greeting ""
